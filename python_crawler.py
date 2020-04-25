69a8f3dc99cfa8e27af27bcdc02120c9bc7d31aa
Jorge-Hernansanz <60103619+Jorge-Hernansanz@users.noreply.github.com>
2020-04-25 18:31:44 +0200

    The loop needs to be checked

--- python_crawler.py
+++ python_crawler.py
@@ -9,7 +9,7 @@
 
 #####
 
-
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common import keys
 import time
@@ -25,19 +25,34 @@ browser.find_element_by_xpath("//a[@class = 'simple-pink-button acceptCookies']"
 
 links = browser.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button' and @class = '']") #Sintax used to find the different links
 
+
+#Creating dictionary to store the data
+data = {}
+data['Title'] = []
+data['Author'] = []
+data['Categories'] = []
+data['Keywords'] = []
+data['Description'] = []
+
+
 for link in links:                     #For each of the links nodes
+    time.sleep(5)
+    print('hola')
     lnk = link.get_attribute('href')   #We have the individual URL at href class so we select it
     print(lnk)
     URL_to_scrap = lnk
     browser.get(URL_to_scrap)          #We go to the designated URL
 
+
     title = browser.find_element_by_xpath("//h2[@class = 'title']").text                                #Extracting tittle of trayectory 
     Author = browser.find_element_by_xpath("//a[@class = 'normal-link author']").text                   #EXtracting the author 
 
+    time.sleep(2)
+
     Categories_list = []
-    Categories = browser.find_elements_by_xpath("//ul//a[@class = 'normal-link']")                      #Syntax used to identify the different links of categoreis that thee may appear 
+    Categories = browser.find_elements_by_xpath("//ul[@class = 'normal-list']//a[@class = 'normal-link']")      #Syntax used to identify the different links of categoreis that thee may appear 
     for string in Categories:
-        filtered_string = string.text #THe string we want appears at the text part
+        filtered_string = string.text                                                                             #The string we want appears at the text part
         Categories_list.append(filtered_string)
     
     Keywords_list = []
@@ -48,18 +63,15 @@ for link in links:                     #For each of the links nodes
 
     Description = browser.find_element_by_xpath("//Div[@class = 'description section']").text
 
-    print(title, "\n", Author, "\n", Categories_list, "\n", Keywords_list, Description)
+    data['Title'].append(title)
+    data['Author'].append(Author)
+    data['Categories'].append(Categories_list)
+    data['Keywords'].append(Keywords_list)
+    data['Description'].append(Description)
 
-    #Now it is left to improve some kind of dataframe or whatever
-    break
+    #print(title, "\n", Author, "\n", Categories_list, "\n", Keywords_list, "\n" ,Description)
 
+    #break
 
-
-
-
-
-
-
-
-#<a class="" href="https://figshare.com/articles/GTT-1-protein-006_dcd/12162405" rel="noopener noreferrer" role="button" target="" title=""><div class="card-item-thumbnail withIcon"><div class="it-preview-placeholder"><div class="it-icon-wrapper"><div class="it-icon dataset"></div></div><div class="it-placeholder-name">dataset</div></div></div><div class="itc-title"><span class="line-clamp last-line line-clamp-defaults">GTT-1-protein-006.dcd</span></div></a>
-#<a class="simple-pink-button acceptCookies" href="#" rel="noopener noreferrer" role="button" target="" title="">Accept cookies</a>
+dataframe = pd.DataFrame(data)                # Creating dataframe
+dataframe.to_csv("~/Desktop/DAtaframe.csv")   #Change to a personalized location
