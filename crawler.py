2635f9c3892451c4b86b02a7f3083f2815eac521
Steven Garcia <sgarcia00@gmail.com>
2020-04-25 22:46:27 +0200

    updating crawler to handle xpath failures

--- crawler.py
+++ crawler.py
@@ -8,14 +8,17 @@ import pandas as pd
 
 import requests
 from bs4 import BeautifulSoup
+from crawler_lib.study_params import Protien, StudyParameters
 
 FIGSHARE_COOKIE_XPATH = "//a[@class = 'simple-pink-button acceptCookies']"
 
 FIGSHARE_ANCHOR_XPATH = "//a[contains(@href,'https://') and @role = 'button' and @class = '']"
 
-INFINI_SCROLL_WAIT_TIME = 5
+DEFAULT_INFINI_SCROLL_TIMEOUT = 5
 
 DEFAULT_IMPLICIT_WAIT_TIME = 30
+
+FIGSHARE_SELENIUM_LINKS = []
 
 
 def run(url: str) -> dict:
@@ -30,6 +33,7 @@ def run(url: str) -> dict:
     append_bs_parsing(messages, initial_response)
     append_selenium_parsing(messages, url, args)
     # parse_secondary_link(driver)
+    # output_pandas_csv()
     return {"messages": messages}
 
 
@@ -43,39 +47,36 @@ def append_bs_parsing(messages, initial_response):
 
         hrefs_reg = []
         for item in hrefs:
-            searchObj = re.search(r"http.*", item, re.I)
-            if searchObj:
-                hrefs_reg.append(searchObj.group())
+            search_obj = re.search(r"http.*", item, re.I)
+            if search_obj:
+                hrefs_reg.append(search_obj.group())
 
         messages.append({"soup": "parsed",
                          "links": hrefs,
                          "urls": hrefs_reg})
-    except:
+    except Exception:
         soup = {"status": "failed to parse"}
         messages.append({"soup": soup, "exception": traceback.format_exc()})
 
 
-def append_selenium_parsing(messages, url, args):
+def append_selenium_parsing(messages: list, url: str, args: argparse.Namespace):
     try:
-        links = prepare_selenium_response(url, args)
+        prepare_selenium_response(url, args)
         messages.append({"selenium": "parsed",
-                         "links": links})
+                         "links": FIGSHARE_SELENIUM_LINKS,
+                         "total": str(len(FIGSHARE_SELENIUM_LINKS))})
     except:
         selenium = {"status": "failed to parse"}
         messages.append({"selenium": selenium, "exception": traceback.format_exc()})
 
 
-def prepare_selenium_response(url: str, args: argparse.Namespace) -> list:
+def prepare_selenium_response(url: str, args: argparse.Namespace) -> None:
     driver = build_webdriver(args)
     driver.get(url)
     driver.implicitly_wait(DEFAULT_IMPLICIT_WAIT_TIME)
     accept_cookies(driver)
 
-    links = scroll_to_bottom_and_get_links(driver)
-    if links:
-        print("parsing link hrefs")
-        return [x.get_attribute('href') for x in links]
-    return []
+    scroll_to_bottom_and_get_links(driver)
 
 
 def accept_cookies(driver) -> None:
@@ -85,37 +86,49 @@ def accept_cookies(driver) -> None:
         element.click()
 
 
-def scroll_to_bottom_and_get_links(driver) -> list:
-    links_found = get_links_in_browser(driver, [])
+def scroll_to_bottom_and_get_links(driver) -> None:
     seed_height = driver.execute_script("return document.body.scrollHeight")
-    return scroll_down_finding_links(driver, seed_height, links_found)
+    scroll_down_finding_links(driver, seed_height)
 
 
-def scroll_down_finding_links(driver, height, links_found) -> list:
+def update_figshare_links(links_found):
+    for link in links_found:
+        link_string = link.get_attribute('href')
+        if link_string not in FIGSHARE_SELENIUM_LINKS:
+            FIGSHARE_SELENIUM_LINKS.append(link_string)
+
+
+def scroll_down_finding_links(driver, height) -> None:
     driver.execute_script("scroll(0, 250);")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    links_found = get_links_in_browser(driver, links_found)
-    print("new links found: ", len(links_found))
-    driver.implicitly_wait(INFINI_SCROLL_WAIT_TIME)
+    get_links_in_browser(driver, 0)
+    print("current links ", len(FIGSHARE_SELENIUM_LINKS))
     executed_height = driver.execute_script("return document.body.scrollHeight")
     if executed_height == height:
-        print("final link total: ", str(len(links_found)))
-        return links_found
+        return
     print("scrolling to bottom, current height: {}, previous height: {}".format(executed_height, height))
-    time.sleep(INFINI_SCROLL_WAIT_TIME)
-    scroll_down_finding_links(driver, executed_height, links_found)
+    time.sleep(DEFAULT_INFINI_SCROLL_TIMEOUT)
+    scroll_down_finding_links(driver, executed_height)
 
 
-def get_links_in_browser(driver, link_list: list) -> list:
-    links = driver.find_elements_by_xpath(FIGSHARE_ANCHOR_XPATH)
-    if links:
-        for link in links:
-            if link not in link_list:
-                link_list.append(link)
-    return link_list
+def get_links_in_browser(driver, current_attempt: int) -> None:
+    try:
+        if current_attempt > 10:
+            print("giving up!")
+            return
+        links = driver.find_elements_by_xpath(FIGSHARE_ANCHOR_XPATH)
+        if links:
+            update_figshare_links(links)
+    except Exception as e:
+        current_attempt += 1
+        error_msg = "could not parse element it on attempt: " \
+                    + str(current_attempt) \
+                    + ", retrying. error:"
+        print(error_msg, str(e))
+        get_links_in_browser(driver, current_attempt)
 
 
-def build_webdriver(args):
+def build_webdriver(args: argparse.Namespace):
     from selenium import webdriver
     driver = None
     if args.webdriver == "firefox":
@@ -146,25 +159,24 @@ def parse_secondary_link(links: list, browser) -> None:
     tag_section_xpath = "//div[@class = 'tags section']//a[@class = 'tag-wrap']"
     if links:
         for link in links:
+            study = StudyParameters()
             lnk = link.get_attribute('href')
-            browser.get(lnk)  # We go to the designated URL
+            browser.get(lnk)
             title = browser.find_element_by_xpath(title_xpath).text  # Extracting tittle of trayectory
             author = browser.find_element_by_xpath(author_xpath).text  # EXtracting the author
-
-            categories_list = []
+            study.add_authors(author)
+            study.add_title(title)
             categories = browser.find_elements_by_xpath(normal_link_string)
             for string in categories:
                 filtered_string = string.text
-                categories_list.append(filtered_string)
+                study.add_category(filtered_string)
 
             keywords_list = []
-            tag_secion = tag_section_xpath
-            Keywords = browser.find_elements_by_xpath(
-                tag_secion)  # Same that for categories
-            for string in Keywords:
+            tag_section = tag_section_xpath
+            keywords = browser.find_elements_by_xpath(tag_section)  # Same that for categories
+            for string in keywords:
                 filtered_string = string.get_attribute("title")
                 keywords_list.append(filtered_string)
-            print(title, "\n", author, "\n", categories_list, "\n", keywords_list)
 
 
 def url_response(messages: list, url: str) -> requests.Response:
--- crawler_lib/study_params.py
+++ crawler_lib/study_params.py
@@ -3,24 +3,38 @@ class Protien:
         self.file_list = file_list
 
     def add_file(self, file: str) -> None:
-        pass
+        if file not in self.file_list:
+            self.file_list.append(file)
 
 
 class StudyParameters:
-    def __init__(self, description=None, protien_list=None, author_list=None, keywords=None):
+    def __init__(self, title=None, description=None, protien_list=None,
+                 author_list=None, keywords=None, categories=None):
         self.description = description
         self.protein_list = protien_list
         self.author_list = author_list
         self.keywords = keywords
+        self.categories = categories
+        self.title = title
+
+    def add_title(self, title: str) -> None:
+        self.title = title
 
     def add_description(self, description: str) -> None:
-        pass
+        self.description = description
 
     def add_protien(self, protien: Protien) -> None:
-        pass
+        if protien not in self.protein_list:
+            self.protein_list.append(protien)
 
     def add_authors(self, author: str) -> None:
-        pass
+        if author not in self.author_list:
+            self.author_list.append(author)
 
     def add_keyword(self, keyword: str) -> None:
-        pass
+        if keyword not in self.keywords:
+            self.keywords.append(keyword)
+
+    def add_category(self, category: str) -> None:
+        if category not in self.categories:
+            self.categories.append(category)
