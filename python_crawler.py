3ce11c769ec3cd21645293cfc202b8a981996192
Jorge-Hernansanz <60103619+Jorge-Hernansanz@users.noreply.github.com>
2020-04-25 15:02:19 +0200

    improvement
    Finds tittle, author, categories and keywords of all links detected.
    No implementation of dataframe to store data yet

--- python_crawler.py
+++ python_crawler.py
@@ -1,21 +1,58 @@
+
+import sys
+sys.path.insert(0, 'usr/lib/chromium-browser/chromedriver')
+from selenium import webdriver
+chrome_options = webdriver.Chrome_options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+
+#####
+
+
 from selenium import webdriver
 from selenium.webdriver.common import keys
 import time
 
 URL1 = "https://figshare.com/search?q=.dcd&searchMode=1"
 
-browser = webdriver.Chrome()
-browser.get(URL1)
+browser = webdriver.Chrome('chromedriver',chrome_options=chrome_options)                        #Connects to chrome
+browser.get(URL1)                                                                               # Opens in chrome the URL
     
 time.sleep(2)
 
-browser.find_element_by_xpath("//a[@class = 'simple-pink-button acceptCookies']").click()
+browser.find_element_by_xpath("//a[@class = 'simple-pink-button acceptCookies']").click()                      #Accepts cookies (important to be able to continue)
 
-links = browser.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button']")
+links = browser.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button' and @class = '']") #Sintax used to find the different links
 
-for link in links:
-    lnk = link.get_attribute('href')
+for link in links:                     #For each of the links nodes
+    lnk = link.get_attribute('href')   #We have the individual URL at href class so we select it
     print(lnk)
+    URL_to_scrap = lnk
+    browser.get(URL_to_scrap)          #We go to the designated URL
+
+    title = browser.find_element_by_xpath("//h2[@class = 'title']").text                                #Extracting tittle of trayectory 
+    Author = browser.find_element_by_xpath("//a[@class = 'normal-link author']").text                   #EXtracting the author 
+
+    Categories_list = []
+    Categories = browser.find_elements_by_xpath("//ul//a[@class = 'normal-link']")                      #Syntax used to identify the different links of categoreis that thee may appear 
+    for string in Categories:
+        filtered_string = string.text #THe string we want appears at the text part
+        Categories_list.append(filtered_string)
+    
+    Keywords_list = []
+    Keywords = browser.find_elements_by_xpath("//div[@class = 'tags section']//a[@class = 'tag-wrap']") #Same that for categories
+    for string in Keywords:
+        filtered_string = string.get_attribute("title")                                                 #The string we are interested in appears at the tittle part of the node
+        Keywords_list.append(filtered_string)
+
+    print(title, "\n", Author, "\n", Categories_list, "\n", Keywords_list)
+
+    #Now it is left to improve some kind of dataframe or whatever
+    break
+    
+
 
 
 
