20fd2dec7817a5b02d0ce7011209b68d1cd5c25a
Steven Garcia <sgarcia00@gmail.com>
2020-04-25 18:30:35 +0200

    update original crawler to add method for infinite scroll, add arguments to choose webdriver type

--- crawler.py
+++ crawler.py
@@ -2,13 +2,11 @@
 import argparse
 import json
 import re
-import sys
 import traceback
+import time
 
 import requests
 from bs4 import BeautifulSoup
-
-SITE_STRING = "https://{base_url}/{query_params}"
 
 
 def run(url: str) -> dict:
@@ -21,7 +19,8 @@ def run(url: str) -> dict:
         return {"messages": messages}
 
     append_bs_parsing(messages, initial_response)
-    append_selenium_parsing(messages, url)
+    append_selenium_parsing(messages, url, args)
+    #parse_secondary_link(driver)
     return {"messages": messages}
 
 
@@ -46,29 +45,93 @@ def append_bs_parsing(messages, initial_response):
         soup = {"status": "failed to parse"}
         messages.append({"soup": soup, "exception": traceback.format_exc()})
 
-def append_selenium_parsing(messages, url):
+def append_selenium_parsing(messages, url, args):
     try:
         messages.append({"selenium": "parsed",
-                     "links": prepare_selenium_response(url)})
+                     "links": prepare_selenium_response(url, args)})
     except:
         selenium = {"status": "failed to parse"}
         messages.append({"selenium": selenium, "exception": traceback.format_exc()})
 
-def prepare_selenium_response(url:str)->list:
-    #TODO:cleanup webdriver methods
-    from selenium import webdriver
-    driver = webdriver.Firefox()
+def prepare_selenium_response(url:str, args:argparse.Namespace)->list:
+    driver = build_webdriver(args)
     driver.get(url)
     driver.implicitly_wait(30)
-    elements = driver.find_elements_by_class_name("item-thumb-card")
-    # selenium_links = driver.execute_script('var bob = document.getElementsByClassName("item-thumb-card")``V;Object.keys(bob).forEach(function(a){bob[a].childNodes.forEach(function(b){return b.attributes})})')
-    # selenium_links = driver.execute_script('var items = {};Object.keys(items).forEach(function(attrs){console.log(item[attrs]);return items[attrs];});', element)
-    selenium_links = []
-    # for element in elements:
-    java_script = 'var items = document.getElementsByClassName("item-thumb-card");var n = [];for (i=0; i < items.length;i++){var temp = items[i].childNodes[0].getAttribute("href");console.log(temp);n.push(temp);};return n;'
-    selenium_links.append(driver.execute_script(java_script, None))
-    return [x for x in selenium_links]
+    cookie_string = "//a[@class = 'simple-pink-button acceptCookies']"
+    driver.find_element_by_xpath(cookie_string).click()  # Accepts cookies (important to be able to continue)
+    print("accepted cookies")
 
+    #scroll_to_bottom(driver)
+    links = driver.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button' and @class = '']")  # Sintax used to find the different links
+
+    return [x.get_attribute('href') for x in links]
+
+def scroll_to_bottom(driver):
+    seed_height = driver.execute_script("return document.body.scrollHeight")
+    scroll_down(driver, seed_height)
+
+def scroll_down(driver, height):
+    driver.execute_script("scroll(0, 250);")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    driver.implicitly_wait(5)
+    executed_height = driver.execute_script("return document.body.scrollHeight")
+    print(executed_height, height)
+    if (executed_height == height):
+        return
+    time.sleep(5)
+    scroll_down(driver, executed_height)
+
+
+def build_webdriver(args):
+    from selenium import webdriver
+    driver = None
+    if args.webdriver == "firefox":
+        print("starting firefox driver")
+        firefox_options = webdriver.FirefoxOptions()
+        firefox_options.add_argument('--headless')
+        driver = webdriver.Firefox()
+    elif args.webdriver == "chrome":
+        print("starting chrome driver")
+        if args.webdriver_location is None:
+            print("webdriver file location required, please add...")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome(args.webdriver_location, chrome_options=chrome_options)
+    if driver is None:
+        print("could not build webdriver on localhost")
+    return driver
+
+
+def parse_secondary_link(links:list, browser) -> None:
+    print("parsing secondary links")
+    links_string = "//a[contains(@href,'https://') and @role = 'button' and @class = '']"
+    links = browser.find_elements_by_xpath(links_string)  # Sintax used to find the different links
+    if links:
+        for link in links:
+            lnk = link.get_attribute('href')  # We have the individual URL at href class so we select it
+            browser.get(lnk)  # We go to the designated URL
+            title = browser.find_element_by_xpath("//h2[@class = 'title']").text  # Extracting tittle of trayectory
+            Author = browser.find_element_by_xpath("//a[@class = 'normal-link author']").text  # EXtracting the author
+
+            Categories_list = []
+            normal_link_string = "//ul//a[@class = 'normal-link']"
+            Categories = browser.find_elements_by_xpath(
+                normal_link_string)  # Syntax used to identify the different links of categoreis that thee may appear
+            for string in Categories:
+                filtered_string = string.text  # THe string we want appears at the text part
+                Categories_list.append(filtered_string)
+
+            Keywords_list = []
+            tag_secion = "//div[@class = 'tags section']//a[@class = 'tag-wrap']"
+            Keywords = browser.find_elements_by_xpath(
+                tag_secion)  # Same that for categories
+            for string in Keywords:
+                filtered_string = string.get_attribute(
+                    "title")  # The string we are interested in appears at the tittle part of the node
+                Keywords_list.append(filtered_string)
+            print(title, "\n", Author, "\n", Categories_list, "\n", Keywords_list)
 
 def url_response(messages: list, url: str) -> requests.Response:
     try:
@@ -81,8 +144,6 @@ def url_response(messages: list, url: str) -> requests.Response:
 
 
 def main(args: argparse.Namespace) -> None:
-    # TODO: check if desired:
-    # url = SITE_STRING.format(base_url=args.base_url, query_params=args.query_params)
     url = args.url
     try:
         json_response = json.dumps(run(url), indent=4)
@@ -94,8 +155,8 @@ def main(args: argparse.Namespace) -> None:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="command line tool to parse web links for bioinfo links")
 
-    # TODO: check if desired:
-    # parser.add_argument('--query_params', type=str, required=False, help="add your query params here e.g.: ?id=1&name=bob")
     parser.add_argument('--url', type=str, required=True)
+    parser.add_argument('--webdriver', type=str, choices=["chrome", "firefox"], required=True)
+    parser.add_argument('--webdriver_location', type=str, required=False)
     args = parser.parse_args()
     main(args)
