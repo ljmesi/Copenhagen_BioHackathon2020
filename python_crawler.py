3c8ac42c485868d2503f3d3aa1eba29b90bbbc42
Jorge-Hernansanz <60103619+Jorge-Hernansanz@users.noreply.github.com>
2020-04-25 21:31:17 +0200

    Fixed and with pandas dataframe commands

--- python_crawler.py
+++ python_crawler.py
@@ -16,14 +16,16 @@ import time
 
 URL1 = "https://figshare.com/search?q=.dcd&searchMode=1"
 
-browser = webdriver.Chrome()                                                                    #Connects to chrome
-browser.get(URL1)                                                                               # Opens in chrome the URL
+browser1 = webdriver.Chrome()                                                                    #Connects to chrome principal page
+browser2 = webdriver.Chrome()                                                                    #Connects to each link we visit
+
+browser1.get(URL1)                                                                               # Opens in chrome the URL
     
 time.sleep(2)
 
-browser.find_element_by_xpath("//a[@class = 'simple-pink-button acceptCookies']").click()                      #Accepts cookies (important to be able to continue)
+browser1.find_element_by_xpath("//a[@class = 'simple-pink-button acceptCookies']").click()                      #Accepts cookies (important to be able to continue)
 
-links = browser.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button' and @class = '']") #Sintax used to find the different links
+links = browser1.find_elements_by_xpath("//a[contains(@href,'https://') and @role = 'button' and @class = '']") #Sintax used to find the different links
 
 
 #Creating dictionary to store the data
@@ -36,32 +38,33 @@ data['Description'] = []
 
 
 for link in links:                     #For each of the links nodes
-    time.sleep(5)
     print('hola')
+    #time.sleep(10)
     lnk = link.get_attribute('href')   #We have the individual URL at href class so we select it
     print(lnk)
     URL_to_scrap = lnk
-    browser.get(URL_to_scrap)          #We go to the designated URL
+    browser2.get(URL_to_scrap)          #We go to the designated URL
 
 
-    title = browser.find_element_by_xpath("//h2[@class = 'title']").text                                #Extracting tittle of trayectory 
-    Author = browser.find_element_by_xpath("//a[@class = 'normal-link author']").text                   #EXtracting the author 
+    title = browser2.find_element_by_xpath("//h2[@class = 'title']").text 
+    time.sleep(1)                               #Extracting tittle of trayectory 
+    Author = browser2.find_element_by_xpath("//a[@class = 'normal-link author']").text                   #EXtracting the author 
 
     time.sleep(2)
 
     Categories_list = []
-    Categories = browser.find_elements_by_xpath("//ul[@class = 'normal-list']//a[@class = 'normal-link']")      #Syntax used to identify the different links of categoreis that thee may appear 
+    Categories = browser2.find_elements_by_xpath("//ul[@class = 'normal-list']//a[@class = 'normal-link']")      #Syntax used to identify the different links of categoreis that thee may appear 
     for string in Categories:
         filtered_string = string.text                                                                             #The string we want appears at the text part
         Categories_list.append(filtered_string)
     
     Keywords_list = []
-    Keywords = browser.find_elements_by_xpath("//div[@class = 'tags section']//a[@class = 'tag-wrap']") #Same that for categories
+    Keywords = browser2.find_elements_by_xpath("//div[@class = 'tags section']//a[@class = 'tag-wrap']") #Same that for categories
     for string in Keywords:
         filtered_string = string.get_attribute("title")                                                 #The string we are interested in appears at the tittle part of the node
         Keywords_list.append(filtered_string)
 
-    Description = browser.find_element_by_xpath("//Div[@class = 'description section']").text
+    Description = browser2.find_element_by_xpath("//Div[@class = 'description section']").text
 
     data['Title'].append(title)
     data['Author'].append(Author)
