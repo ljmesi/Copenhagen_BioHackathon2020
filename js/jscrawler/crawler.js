de903a79ebb89baaa9e353045cb273666586166f
Steven Garcia <sgarcia00@gmail.com>
2020-04-28 20:34:54 +0200

    js update

--- js/jscrawler/crawler.js
+++ js/jscrawler/crawler.js
@@ -1,8 +1,21 @@
 const puppeteer = require('puppeteer');
-//TODO: move to args
-const url = 'https://figshare.com/search?q=dcd&searchMode=1';
 // const rp = require('request-promise');
 // const $ = require('cheerio');
+const argv = require('yargs')
+        .scriptName("crawler.js")
+        .option('url', {
+            description: 'the url to check',
+            alias: 'u',
+            type: 'string'})
+        .option("format", {
+        description: 'The format to output the results',
+        alias: 'fmt',
+        choices: ["csv", "text"],
+        default: "text",
+        type: 'string'})
+        .help()
+        .alias('help', 'h')
+        .argv;
 console.log("crawler starting ");
 (async () => {
   const browser = await puppeteer.launch({
@@ -10,8 +23,11 @@ console.log("crawler starting ");
     timeout: 10000,
   })
   const page = await browser.newPage()
-  await page.goto(url)
+  await page.goto(argv.url)
   const title = await page.title()
+  if (argv.fmt) {
+      console.log("output fmt: ", argv.fmt);
+  }
   console.log(title)
   await browser.close()
 })()
