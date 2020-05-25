0c042b72a0dfe51c89ec3c2633e80581bfed5ffe
Steven Garcia <sgarcia00@gmail.com>
2020-05-25 23:51:48 +0200

    reverting to old dockerfile for python crawling, adding minimal selenium /beautiful soup template crawling

---
+++ crawler_templates/python/pycrawler.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import json
+import os
+import traceback
+import argparse
+
+import requests
+from bs4 import BeautifulSoup
+from numpy.core.tests.test_scalarinherit import D
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+#from lib.study import Study
+
+
+def run(url: str) -> dict:
+    data = []
+    parse_html(data, url)
+    return {"data": data}
+
+def parse_html(messages: list, url: str):
+    try:
+        driver = prepare_selenium_response(url)
+        bs = BeautifulSoup(driver.page_source, "html.parser")
+        links = bs.findAll('a', href=True)
+        hrefs = []
+        for link in links:
+            hrefs.append(link.attrs["href"])
+
+        messages.append({"soup": "parsed",
+                         "links": hrefs})
+        driver.close()
+    except:
+        selenium = {"status": "failed to parse"}
+        messages.append({"selenium": selenium, "exception": traceback.format_exc()})
+
+
+def prepare_selenium_response(url: str) -> WebDriver:
+    driver = build_webdriver()
+    driver.get(url)
+    driver.implicitly_wait(100)
+    return driver
+
+
+def build_webdriver() -> WebDriver:
+    print("starting chrome driver")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome(options=chrome_options,
+                            executable_path="/usr/src/app/chromedriver",
+                            desired_capabilities=DesiredCapabilities.CHROME)
+
+
+
+def main(args: argparse.Namespace) -> None:
+    url = args.url
+    try:
+        json_response = json.dumps(run(url), indent=4)
+        print(json_response)
+    except Exception as exc:
+        print("failed to run crawler", exc)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="command line tool to parse web content")
+    parser.add_argument('--url', type=str, default=os.environ.get('URL'))
+    args = parser.parse_args()
+    main(args)
---
+++ crawler_templates/python/start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+echo "starting pycralwer"
+export URL='https://google.com'
+python3 pycrawler.py
+echo "ending pycrawler"
